d0eaf94bb0c089263873979367b78e510e9d57f1
vishal gehlot <vishalgehlot334@gmail.com>
2024-12-18 16:53:23 +0530

    created header with responsive

---
+++ src/Components/Body.jsx
@@ -0,0 +1,27 @@
+import Card from "./Card";
+
+const Body = () => {
+  return (
+    <div className="flex flex-wrap ">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default Body;
--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -1,5 +1,23 @@
+import Body from "../Components/Body";
+import Header from "../Components/Header";
+import Sidebar from "../Components/Sidebar";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <section className="w-full h-auto">
+      <div className="w-full h-full">
+        <Header />
+      </div>
+      <div className="w-full h-full flex">
+        <div className="w-2/12 h-screen border border-red-500">
+          <Sidebar />
+        </div>
+        <div className="w-10/12 h-screen border border-red-500">
+          <Body />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
