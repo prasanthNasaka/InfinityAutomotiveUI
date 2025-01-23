f7f94bddb4883c911364168e87b735d238342368
vishal gehlot <vishalgehlot334@gmail.com>
2025-01-23 13:10:28 +0530

    Designed dashboard with responsive

--- src/Components/Upcoming.jsx
+++ src/Components/Upcoming.jsx
@@ -1,20 +1,39 @@
 import Card from "./Card";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const Upcoming = () => {
   return (
-    <div className="flex flex-wrap h-full w-full ">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div className="flex flex-col">
+      <div className="w-full h-24">
+        <Header />
+      </div>
+
+      <div className="flex h-[calc(100vh-6rem)]">
+        <div className="w-2/12 h-full iphone:hidden phone:hidden desk:block lappy:block tab:hidden ">
+          <Sidebar />
+        </div>
+        <div className="w-10/12 p-2 flex flex-wrap h-full overflow-y-scroll">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 };
