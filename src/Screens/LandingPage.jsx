945c374896dd0dcd9170454b78b847d4a87cbd0a
vishal gehlot <vishalgehlot334@gmail.com>
2025-02-05 19:34:05 +0530

    packages, card, landingpage

--- src/Screens/LandingPage.jsx
+++ src/Screens/LandingPage.jsx
@@ -1,20 +1,27 @@
-import Body from "../Components/Body";
 import Newheader from "../Components/Header";
-
+import Live from "../Components/Live";
 // import Sidebar from "../Components/Sidebar";
+
 const LandingPage = () => {
   return (
-    <section className="w-full h-screen flex-grow">
+    <section className="w-full h-screen">
       <div className="h-full flex flex-col">
+        {/* Header Section */}
         <div className="h-24 w-full">
           <Newheader />
         </div>
-        <div className="flex h-[calc(100vh-6rem)]">
-          {/* <div className="w-2/12 h-full iphone:hidden phone:hidden desk:block lappy:block tab:hidden ">
-            <Sidebar />
-          </div> */}
-          <div className="w-10/12 h-full overflow-y-auto flex-grow p-2 tab:w-full iphone:w-full phone:w-full">
-            <Body />
+
+        <div className="flex flex-col h-[calc(100vh-6rem)]">
+          <div className="h-full">
+            <div className="w-full h-1/3 p-2 tab:w-full iphone:w-full phone:w-full">
+              <Live />
+            </div>
+            <div className="w-full h-1/3 p-2 tab:w-full iphone:w-full phone:w-full">
+              <Live />
+            </div>
+            <div className="w-full h-1/3 p-2 tab:w-full iphone:w-full phone:w-full">
+              <Live />
+            </div>
           </div>
         </div>
       </div>
