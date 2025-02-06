bda8308624741fdb17b80db8e67c92aab2991174
vishal gehlot <vishalgehlot334@gmail.com>
2025-02-06 13:32:34 +0530

    created the landing animation and improved header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,54 +1,70 @@
 /* eslint-disable no-unused-vars */
 import flag from "../assets/amon.png";
-import photo from "../assets/flag.jpeg";
 import { IoIosMenu } from "react-icons/io";
-import { FaAngleDown } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
   const navigate = useNavigate();
-  const [isLoggedOut, setIsLoggedOut] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const logOutHandler = () => {
-    localStorage.removeItem("authToken");
-    setIsLoggedOut(true);
-    window.location.href = "/";
-    setIsLoggedOut();
-    navigate("/login");
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <section className="w-full h-full flex justify-between items-center shadow-md text-center font-thin bg-white">
-      <div className="h-full w-full flex items-center phone:w-full iphone:w-full ">
-        <div className="w-full h-full flex items-center  phone:justify-between iphone:justify-between lappy:justify-start  tab:justify-start  desk:justify-start   ">
-          <img
-            className="h-16 w-60 iphone:w-28 object-fit phone:w-36  "
-            src={flag}
-            alt="Flag"
-          />
-        </div>
+    <header className="w-full h-full flex justify-between items-center shadow-md text-center font-thin bg-white p-4">
+      <div className="flex w-full items-center justify-between">
+        <img
+          className="h-16 w-60 object-contain phone:w-36 iphone:w-28"
+          src={flag}
+          alt="Flag"
+        />
+        <Link to="/login">
+          <button
+            type="button"
+            className="text-white bg-cyan-600 font-medium phone:hidden rounded-lg text-sm px-5 py-2.5"
+          >
+            Login
+          </button>
+        </Link>
       </div>
-      <div className="h-full w-1/5 flex justify-center items-center phone:hidden iphone:hidden">
-        <div className="w-full h-full flex justify-around items-center gap-4  ">
-          <div className="   flex items-center phone:hidden iphone:hidden tab:hidden lappy:w-36 desk:w-36 2xl:w-32 tab:w-26 ">
-            <div className="relative group flex justify-center items-center w-32     ">
-              <button>
-                <Link className="text-black" to={"/login"}>
-                  <button
-                    type="button"
-                    className="focus:outline-none text-white bg-cyan-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2  phone:justify-end iphone:justify-end lappy:justify-end  tab:justify-end  desk:justify-end"
-                  >
-                    Login
-                  </button>
-                </Link>
-              </button>
+      <nav className="phone:flex iphone:flex flex items-center phone:w-full phone:justify-end ">
+        <button onClick={toggleMenu} className="text-3xl hidden phone:block">
+          <IoIosMenu />
+        </button>
+      </nav>
+
+      {isMenuOpen && (
+        <div
+          className={`absolute top-0 tab:block right-0 h-fit w-1/2 tab:right-0 tab:w-1/5 tab:h-24 tab:rounded-xl bg-white shadow-lg flex-col items-center gap-6 p-6
+          transition-all duration-1000 ease-in-out transform lappy:hidden ${
+            isMenuOpen
+              ? "translate-x-0 opacity-100 flex"
+              : "translate-x-full opacity-0 hidden"
+          }`}
+        >
+          <div className="flex justify-between">
+            <div className="w-1/2">
+            <AiOutlineClose
+              className="text-black cursor-pointer self-end hover:text-cyan-600"
+              onClick={toggleMenu}
+            />
             </div>
-          
+            
+            <Link to="/login">
+              <button
+                type="button"
+                className="text-white bg-cyan-600 font-medium phone:block rounded-lg text-sm px-5 py-2.5"
+              >
+                Login
+              </button>
+            </Link>
           </div>
         </div>
-      </div>
-    </section>
+      )}
+    </header>
   );
 };
 
--- src/Components/Upcoming.jsx
+++ src/Components/Upcoming.jsx
@@ -1,40 +1,85 @@
 import Card from "./Card";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
+import { useRef } from "react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Upcoming = () => {
-  return (
-    <div className="flex flex-col">
-      <div className="w-full h-24">
-        <Header />
-      </div>
+  const swiperRef = useRef(null);
 
-      <div className="flex h-[calc(100vh-6rem)]">
-        <div className="w-2/12 h-full iphone:hidden phone:hidden desk:block lappy:block tab:hidden ">
-          <Sidebar />
+   return (
+    <section className="p-3 shadow-md rounded-lg bg-black h-fit">
+      <div className="w-full text-white mb-2  font-semibold text-3xl">
+        <span>Upcoming Events</span>
+      </div>
+      <div className="container mx-auto relative bg-black">
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          autoplay={{ delay: 4000 }}
+          speed={700}
+          loop={true}
+          spaceBetween={20}
+          slidesPerView={3}
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+          className="overflow-hidden flex justify-between items-center"
+        >
+          {[...Array(50)].map((_, index) => (
+            <SwiperSlide key={index} className="!w-[300px]flex-shrink-0">
+              <Card className="!w-[300px]" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <div
+          onClick={() => swiperRef.current?.slidePrev()}
+          className="absolute top-1/2 left-0 transform -translate-y-1/2 z-10 w-10 h-10 bg-cyan-600 rounded-full flex items-center justify-center cursor-pointer hover:bg-cyan-500 hover:text-white"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="white"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+            />
+          </svg>
         </div>
-        <div className="w-10/12 p-2 flex flex-wrap h-full overflow-y-scroll">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div
+          onClick={() => swiperRef.current?.slideNext()}
+          className="absolute top-1/2 right-0 transform -translate-y-1/2 z-10 w-10 h-10 bg-cyan-600 rounded-full flex items-center justify-center cursor-pointer hover:bg-cyan-500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="white"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+            />
+          </svg>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
