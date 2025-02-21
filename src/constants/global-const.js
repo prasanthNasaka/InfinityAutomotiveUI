adcfa68748a299fbb271d69329d918d16441d14c
Home <home@Homes-MacBook-Pro.local>
2025-02-21 19:09:18 +0530

    put api integrated in driver and vehicle

--- src/constants/global-const.js
+++ src/constants/global-const.js
@@ -1,2 +1,2 @@
-export  const BASE_URL = "https://c4pfntkn-7206.inc1.devtunnels.ms";
+export  const BASE_URL =      "https://localhost:7206"   //"https://c4pfntkn-7206.inc1.devtunnels.ms";  
 export const IMAGE_URL= `${BASE_URL}/images/`
