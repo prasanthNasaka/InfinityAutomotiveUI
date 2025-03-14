207c501eed102c79ef7615706e9433ebdea8d776
vishal gehlot <vishalgehlot334@gmail.com>
2025-03-14 10:06:27 +0530

    card camponent

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,6 +3,7 @@ import { Calendar, Flag, MapPin, Timer } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { IMAGE_URL } from "../constants/global-const";
+import { TfiCup } from "react-icons/tfi";
 
 const Card = ({ event, type }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -25,35 +26,66 @@ const Card = ({ event, type }) => {
   return (
     <>
       <div
-        className="border-4 border-red-800 rounded-lg overflow-hidden shadow-lg cursor-pointer bg-white"
+        
+          
+        className="min-w-max h-full  rounded-lg overflow-hidden shadow-lg cursor-pointer bg-white"
         onClick={handleClick}
       >
-        <img
-          src={
-            `${IMAGE_URL}${event.banner}` || `https://via.placeholder.com/800`
-          } // Placeholder for no image
-          alt={event.eventname}
-          className="w-full h-48 object-cover border-4 border-yellow-500"
-        />
+        <div
+         
+        >
+          {type === "live" && (
+            <img
+              src={`${IMAGE_URL}${event.banner}`}
+              alt={event.eventname}
+              className="w-96 h-48 object-fill"
+              onError={(e) => {
+                e.target.onerror = null;
+                e.target.src =
+                  "https://chronopulse-pulse.s3.ap-south-1.amazonaws.com/MapFiles/Events/469.png";
+              }}
+            />
+          )}
+        </div>
+
+        {type === "upcoming" && (
+          <img
+            src={`${IMAGE_URL}${event.banner}`}
+            alt={event.eventname}
+            className="w-96 h-48 object-cover border"
+            onError={(e) => {
+              e.target.onerror = null;
+              e.target.src =
+                "https://chronopulse-pulse.s3.ap-south-1.amazonaws.com/MapFiles/Events/466.png";
+            }}
+          />
+        )}
+
+        {type === "completed" && (
+          <img
+            src={`${IMAGE_URL}${event.banner}`}
+            alt={event.eventname}
+            className="w-96 h-48 object-cover"
+            onError={(e) => {
+              e.target.onerror = null;
+              e.target.src =
+                "https://chronopulse-pulse.s3.ap-south-1.amazonaws.com/MapFiles/Events/433.png";
+            }}
+          />
+        )}
+
         {/* {event.banner} */}
         <div className="p-4">
           <h3 className="text-xl font-bold">{event.eventname}</h3>
           <p className="text-gray-600 flex items-center">
-            <MapPin className="mr-2 border-4 border-green-800" size={16} />
+            <MapPin className="mr-2 " size={20} />
             {event.location}
           </p>
           {type === "live" && (
             <>
               <p className="text-gray-600 flex items-center">
-                <Timer className="mr-2 border-4 border-blue-800" size={16} />
+                <Timer className="mr-2 " size={20} />
                 Lap {event.currentLap} of {event.totalLaps}
-              </p>
-
-              <p className="text-gray-600 flex items-center">
-                <Calendar className="mr-2" size={16} />
-                Start Date: {formatDate(event.startdate)}{" "}
-                {/* Displaying start date */} - End Date:{" "}
-                {formatDate(event.enddate)} {/* Displaying end date */}
               </p>
             </>
           )}
@@ -61,15 +93,28 @@ const Card = ({ event, type }) => {
             <p className="text-gray-600 flex items-center">
               <Calendar className="mr-2" size={16} />
               {formatDate(event.startdate)} {/* Displaying date only */}
+              <p className="text-gray-600  items-center flex text-wrap">
+                <Calendar className="mr-2" size={20} />
+                Start Date: {formatDate(event.startdate)}{" "}
+                {/* Displaying start date */} - End Date:{" "}
+                {formatDate(event.enddate)} {/* Displaying end date */}
+              </p>
             </p>
           )}
 
           {type === "completed" && (
-            <p className="text-gray-600 flex items-center">
-              <Flag className="mr-2" size={16} />
-              Completed on {formatDate(event.startdate)}{" "}
-              {/* Displaying date only */}
-            </p>
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="text-gray-600 flex items-center">
+                  <Flag className="mr-2" size={16} />
+                  Completed on {formatDate(event.startdate)}{" "}
+                  {/* Displaying date only */}
+                </p>
+              </div>
+              <div className="text-4xl text-yellow-600">
+                <TfiCup />
+              </div>
+            </div>
           )}
         </div>
       </div>
@@ -103,14 +148,44 @@ const Card = ({ event, type }) => {
               </button>
             </div>
 
-            <img
-              src={
-                `${IMAGE_URL}${event.banner}` ||
-                "https://images.unsplash.com/photo-1741557571786-e922da981949?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-              }
-              alt={event.eventname}
-              className="w-full h-48 object-cover mb-4 border-4 border-red-800"
-            />
+            {type === "live" && (
+              <img
+                src={`${IMAGE_URL}${event.banner}`}
+                alt={event.eventname}
+                className="w-full h-48 object-fill"
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src =
+                    "https://chronopulse-pulse.s3.ap-south-1.amazonaws.com/MapFiles/Events/469.png";
+                }}
+              />
+            )}
+
+            {type === "upcoming" && (
+              <img
+                src={`${IMAGE_URL}${event.banner}`}
+                alt={event.eventname}
+                className="w-full rounded-lg h-48 object-fill"
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src =
+                    "https://chronopulse-pulse.s3.ap-south-1.amazonaws.com/MapFiles/Events/466.png";
+                }}
+              />
+            )}
+
+            {type === "completed" && (
+              <img
+                src={`${IMAGE_URL}${event.banner}`}
+                alt={event.eventname}
+                className="w-full h-48 rounded-lg object-fill"
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src =
+                    "https://chronopulse-pulse.s3.ap-south-1.amazonaws.com/MapFiles/Events/433.png";
+                }}
+              />
+            )}
 
             <p className="text-gray-600 mb-2">
               <MapPin
